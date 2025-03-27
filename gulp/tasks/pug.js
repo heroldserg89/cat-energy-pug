724eb006c4f3130d6af42e1072860ad8974f1978
herold89 <heroldserg89@gmail.com>
2025-03-28 00:32:05 +0300

    Добавляет разметку списка товара

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,12 +2,17 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import pug from 'gulp-pug';
 import cached from 'gulp-cached';
+import formatHTML from 'gulp-format-html';
 import config from '../config.js';
 
 export const pugBuild = () => gulp
   .src(`${config.src.pug}/*.pug`)
   .pipe(plumber())
-  .pipe(pug({ pretty: true }))
+  .pipe(pug())
+  .pipe(formatHTML({
+    end_with_newline: true,
+    unformatted: ['br'],
+  }))
   .pipe(cached('pug'))
   .pipe(gulp.dest('build'));
 
